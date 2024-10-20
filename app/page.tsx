2d8679d19ac5a028d9b05faf0a2bd53c929fbc9b
Kennedy <tmalonga737@gmail.com>
2024-10-20 06:55:35 +0200

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="underline text-white text-3xl">Home</h1>
+      <Button>Click Me</Button>
+    </div>
+      );
+}
---
+++ components/PasskeyModel.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PasskeyModel = () => {
+  return (
+    <div>PasskeyModel</div>
+  )
+}
+
+export default PasskeyModel
