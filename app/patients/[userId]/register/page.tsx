b13113ed5429c996cddec5962d8aec15f05007cd
ZuchiSpeed <zuchi737@gmail.com>
2024-11-22 11:08:07 +0200

    added sentry for error handling

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,10 +1,14 @@
-import RegisterForm from '@/components/forms/RegisterForm'
-import { getUser } from '@/lib/actions/patient.actions'
-import Image from 'next/image'
-import React from 'react'
+import RegisterForm from "@/components/forms/RegisterForm";
+import { getUser } from "@/lib/actions/patient.actions";
+import Image from "next/image";
+import React from "react";
+
+import * as Sentry from "@sentry/nextjs";
 
 const Register = async ({ params: { userId } }: SearchParamProps) => {
-  const user = await getUser(userId)
+  const user = await getUser(userId);
+
+  Sentry.metrics.set("user_view_register", user.name);
 
   return (
     <div className="flex h-screen max-h-screen">
@@ -32,7 +36,7 @@ const Register = async ({ params: { userId } }: SearchParamProps) => {
         className="side-img max-w-[390px]"
       />
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1b9bb384edd59ef96b5180dd0c74e3c0@o4507875522052096.ingest.us.sentry.io/4508286033068032",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
